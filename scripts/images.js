5b4dcd4c85de70ee96c97305685817979c29c54b
Patrick Conrey <patrickconrey@gmail.com>
2019-09-28 18:34:54 -0400

    new content and pug

--- scripts/images.js
+++ scripts/images.js
@@ -3,62 +3,90 @@ const fs = require('fs')
 const filter = require("lodash/filter")
 
 const imagePath = "./img"
-const processedSuffix = "_blur"
+const blurredSuffix = "_blur"
+const scaledSuffix = "_scaled"
 const blurConstant = 25
+const maximumAllowedDimension = 1000
 
 const getFileExtension = fileName => fileName.split(".")[1]
+
 const getFileNameWithoutExtension = fileName => fileName.split(".")[0]
 
 const getImageFiles = () => {
     const fileNames = fs.readdirSync(imagePath)
-
     const imageFileNames = filter(fileNames, name => {
         const extension = getFileExtension(name)
         return extension === "jpg" || extension === "png"
     })
-
-    console.warn(`There are ${imageFileNames.length} files.`)
+    console.warn(`There are ${imageFileNames.length} image files to consider.`)
 
     return imageFileNames
 }
 
 const scaleAndBlurImage = async fileName => {
     try {
-
+        const pathToImage = `${imagePath}/${fileName}`
         const imageName = getFileNameWithoutExtension(fileName)
-        const image = await jimp.read(`${imagePath}/${fileName}`)
+        const imageExtension = getFileExtension(fileName)
+        const image = await jimp.read(pathToImage)
+
+        const imageHeight = image.bitmap.height
+        const imageWidth = image.bitmap.width
+        const largestImageDimension = imageHeight > imageWidth
+            ? imageHeight
+            : imageWidth
         
-        const fileExtension = getFileExtension(fileName)
-        const newFilePath = `${imagePath}/${imageName}${processedSuffix}.${fileExtension}`
+        if (largestImageDimension > maximumAllowedDimension) {
+            try {
+                const imageScaleFactor = parseFloat((maximumAllowedDimension / largestImageDimension).toFixed(2))
+                console.log(`Scaling original ${pathToImage} by ${imageScaleFactor}`)
+                image.scale(imageScaleFactor)
+
+                const scaledFilePath = `${imagePath}/${imageName}${scaledSuffix}.${imageExtension}`
+                image.write(scaledFilePath)
+            } catch (error) {
+                console.error("⚠️  Error scaling original image: ", error.message)
+            }
+        }
         
-        image.blur(blurConstant, () => console.log(`Processed: ${fileName} -> ${newFilePath}`))
-        image.resize(250, jimp.AUTO)
-        image.write(newFilePath)
+        try {
+            image.resize(250, jimp.AUTO)
+            const blurredFilePath = `${imagePath}/${imageName}${blurredSuffix}.${imageExtension}`
+            image.blur(blurConstant, () => console.log(`Blurring and scaling ${pathToImage} -> ${blurredFilePath}`))
+            image.write(blurredFilePath)
+        } catch (error) {
+            console.error("⚠️  Error blurring and scaling image: ", error.message)
+        }
     } catch (error) {
-        console.error("Error manipulating image: ", error.message)
+        console.error("⚠️  Error reading image: ", error.message)
     }
 }
 
-const removeAllBluredImages = imageFileNames => {
-    const imagesToBeRemoved = filter(imageFileNames, name => /^^\S*_blur{1,}.(jpg|png)$/.test(name))
+const removeAllStaleImages = imageFileNames => {
+    const imagesToBeRemoved = filter(imageFileNames, name => /^^\S*_blur{1,}.(jpg|png)$/.test(name) || /^^\S*_scaled{1,}.(jpg|png)$/.test(name))
+    console.warn(`\n🗑️  Will remove ${imagesToBeRemoved.length} images.`)
 
-    console.warn(`Will remove ${imagesToBeRemoved.length} images.`)
     imagesToBeRemoved.forEach(imageName => {
         const pathToImage = `${imagePath}/${imageName}`
         try {
             fs.unlinkSync(pathToImage)
             console.warn("Removed ", pathToImage)
         } catch (error) {
-            console.error("Error removing file: ", error.message)
+            console.error("⚠️  Error removing file: ", error.message)
         }
     })
 }
 
 const main = async () => {
     try {
-        const imageFileNames = getImageFiles()
-        removeAllBluredImages(imageFileNames)
-        await imageFileNames.forEach(async fileName => {
+        console.log("\n🎨 Will begin processing images.\n")
+
+        const allAvailableImageFileNames = getImageFiles()
+        removeAllStaleImages(allAvailableImageFileNames)
+
+        console.log("\n🖨️  Scaling and blurring images.")
+        const imageFileNamesForProcessing = getImageFiles()
+        await imageFileNamesForProcessing.forEach(async fileName => {
             await scaleAndBlurImage(fileName)
         });
     } catch (error) {
